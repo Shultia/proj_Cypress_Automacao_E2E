fb928d0102eb289ce4ce2d4e00cd72d8c15cfe61
Thiago Cordeiro Dantas <thiagcordeirodantas@gmail.com>
2022-10-30 16:30:17 -0300

    Ajustando

--- cypress/e2e/1-getting-started/alura-busca.cy.js
+++ cypress/e2e/1-getting-started/alura-busca.cy.js
@@ -7,8 +7,8 @@ describe('alura busca cursos', () => {
     it('Buscar curso de Java', () => {
         cy.get('#header-barraBusca-form-campoBusca').type('java');
         cy.get('.header-barraBusca-form-submit').click();
-        cy.get(':nth-child(4) > .busca-resultado-link > .busca-resultado-container > .busca-resultado-nome')
-        .should('have.text', 'Formação Java e Orientação a Objetos');
-    })
+        cy.get('h4.busca-resultado-nome')
+        .should('contain', 'Formação Java e Orientação a Objetos');
+})
 
 })
